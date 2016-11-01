12015964ea64e7f70b39dcd5fb385dc28b2cc516
Greg Deane <greg.james.deane@zalando.de>
2016-11-01 11:54:28 +0100

    refactor(users): switch to bindActionCreators style

--- src/components/user-creation/UserCreationContainer.js
+++ src/components/user-creation/UserCreationContainer.js
@@ -1,17 +1,18 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import UserDetailsForm from './UserDetailsForm';
-import { updateUserDetails } from '../../actions/user-details.actions';
+import * as userActions from '../../actions/user-details.actions';
 
 const UserCreationContainer = (props) => {
   return (
     <div className="dc-row dc-block-grid--small-1 dc-block-grid--medium-2 dc-block-grid--large-3">
       {
         props.userDetailsShown &&
-        <UserDetailsForm heading="Create User"
+        <UserDetailsForm heading="New User"
                          userDetails={props.userDetails}
-                         updateUserDetails={props.updateUserDetails} />
+                         updateUserDetails={props.actions.updateUserDetails} />
       }
     </div>
   );
@@ -20,7 +21,7 @@ const UserCreationContainer = (props) => {
 UserCreationContainer.propTypes = {
   admins: PropTypes.array.isRequired,
   userDetails: PropTypes.object.isRequired,
-  updateUserDetails: PropTypes.func.isRequired
+  actions: PropTypes.object.isRequired
 };
 
 const mapStateToProps = (state) => ({
@@ -28,13 +29,9 @@ const mapStateToProps = (state) => ({
   userDetailsShown: state.visibility.userDetailsShown
 });
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    updateUserDetails: (userDetails, name, value) => {
-      dispatch(updateUserDetails(userDetails, name, value));
-    }
-  };
-};
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(userActions, dispatch)
+});
 
 export default connect(
   mapStateToProps,
