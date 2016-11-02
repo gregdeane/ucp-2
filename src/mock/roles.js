5bad554fe54a32a480efb56047c3ab093b251d18
Greg Deane <greg.james.deane@zalando.de>
2016-11-02 13:59:18 +0100

    refactor(roles): add roles mock wip

---
+++ src/mock/roles.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    "id": 1,
+    "roles": [
+      {
+        "name": "Reviewer",
+        "roles": [
+          {
+            "id": 1,
+            "name": "cms.edit",
+            "moduleId": 1
+          },
+          {
+            "id": 3,
+            "name": "cms.view",
+            "moduleId": 1
+          },
+          {
+            "id": 4,
+            "name": "cms.review",
+            "moduleId": 1
+          }
+        ]
+      }
+    ]
+  }
+];
